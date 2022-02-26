ae699b26c887131e8fa8eab1a1a579acc0bd4a7c
Ali Ramazon <mr.safarali@gmail.com>
2022-02-25 21:51:20 -0700

    Add Rollup config

---
+++ src/Button/Button.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react';
+import { ButtonProps } from './ButtonProps';
+
+const Button: FC<ButtonProps> = ({ size }) => {
+    return <button></button>;
+};
+
+export default Button;
