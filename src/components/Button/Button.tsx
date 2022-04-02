a19706f6f0d22e778006b909e05b8c530e31c5bb
aliramazon <mr.safarali@gmail.com>
2022-04-02 14:58:55 -0600

    Change package name

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,20 @@
 import React, { FC } from 'react';
-import styled, { css } from 'styled-components';
+import styled, { css, createGlobalStyle } from 'styled-components';
 import { ButtonProps, ButtonSize } from './ButtonProps';
+
+const GlobalStyle = createGlobalStyle`
+    *,
+    *::after,
+    *::before {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+    outline: none;
+    }
+
+    html {
+        font-size: 62.5%;
+    }`;
 
 const ButtonBase = styled.button<{ size?: ButtonSize }>`
     font-size: 20px;
